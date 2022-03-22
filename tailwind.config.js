544dae8311d53e5a8af867fecfddab2911ceb9ae
ruj <214782@bk.ru>
2022-03-22 15:23:20 -0300

    feat: font proxima-nova

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   content: ['./src/{pages,components}/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {}
+    extend: {
+      colors: {},
+      fontFamily: {
+        'proxima-nova': ['Proxima Nova', 'sans-serif']
+      }
+    }
   },
   variants: {
     extend: {}
