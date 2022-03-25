b83de88b1105840fb5c0956bd62a59124bc3f487
ruj <214782@bk.ru>
2022-03-25 01:07:38 -0300

    chore: larger title and forwarding to the home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,10 @@ import SwitchTheme from './SwitchTheme'
 
 const Header = ({ title }: { title: string }) => {
   return (
-    <div className="w-full px-16 md:px-32 xl:px-64 2xl:px-96 py-6 flex justify-between">
-      <h1 className="text-xl font-bold">{title}</h1>
+    <div className="w-full px-16 md:px-32 xl:px-64 2xl:px-96 py-6 flex justify-between items-center">
+      <h1 className="text-3xl font-extrabold hover:text-zinc-600 dark:hover:text-zinc-300">
+        <a href="/">{title}</a>
+      </h1>
       <SwitchTheme />
     </div>
   )
