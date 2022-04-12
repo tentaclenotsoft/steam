7104662f43ff84c2adb8232657cdf6bb44ef3e00
ruj <214782@bk.ru>
2022-04-12 04:38:30 -0300

    chore: slight fade

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,19 +2,24 @@ import React from 'react'
 
 const Footer = ({ hoverTextStyle }: { hoverTextStyle: string }) => {
   return (
-    <div className="mx-5">
-      <div className="flex flex-col mx-auto py-10 text-center text-sm font-light tracking-wide">
-        <span>
-          {new Date().getFullYear()} ·{' '}
-          <a href="/tentaclesoft" className={`font-semibold ${hoverTextStyle}`}>
-            Tentaclesoft
-          </a>
-          . All right reserved
-        </span>
-        <span className="text-zinc-500 dark:text-zinc-400 text-xs">
-          This site is not affiliated with Valve, Steam, or any of their
-          partners.
-        </span>
+    <div className="bg-gradient-to-t from-zinc-100 dark:from-zinc-700 to-transparent">
+      <div className="mx-5">
+        <div className="flex flex-col mx-auto py-10 text-center text-sm font-light tracking-wide">
+          <span>
+            {new Date().getFullYear()} ·{' '}
+            <a
+              href="/tentaclesoft"
+              className={`font-semibold ${hoverTextStyle}`}
+            >
+              Tentaclesoft
+            </a>
+            . All right reserved
+          </span>
+          <span className="text-zinc-500 dark:text-zinc-400 text-xs">
+            This site is not affiliated with Valve, Steam, or any of their
+            partners.
+          </span>
+        </div>
       </div>
     </div>
   )
