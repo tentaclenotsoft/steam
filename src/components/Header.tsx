a1c6c5802994f721cae1254996702dea8fdfb3db
ruj <214782@bk.ru>
2022-04-16 23:47:12 -0300

    feat: pages in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,29 @@ import Link from 'next/link'
 
 import SwitchTheme from './SwitchTheme'
 
-const Header = ({ title }: { title: string }) => {
+const Header = ({
+  title,
+  pages
+}: {
+  title: string
+  pages?: [{ title: string; path: string }]
+}) => {
   return (
     <div className="w-full px-16 md:px-32 xl:px-64 2xl:px-96 py-6 flex justify-between items-center">
       <h1 className="text-3xl font-extrabold hover:text-zinc-600 dark:hover:text-zinc-300">
         <Link href="/">{title}</Link>
       </h1>
-      <SwitchTheme />
+      <div className="flex items-center space-x-8">
+        {pages?.map((page, index) => (
+          <div
+            key={index}
+            className="text-xl font-semibold hover:text-zinc-600 dark:hover:text-zinc-300"
+          >
+            <Link href={page.path}>{page.title}</Link>
+          </div>
+        ))}
+        <SwitchTheme />
+      </div>
     </div>
   )
 }
