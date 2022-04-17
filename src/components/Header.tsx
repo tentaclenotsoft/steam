62a37bd99860be3c8697551357fcd5b474fcf69a
ruj <214782@bk.ru>
2022-04-16 22:55:45 -0300

    fix: next link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import Link from 'next/link'
+
 import SwitchTheme from './SwitchTheme'
 
 const Header = ({ title }: { title: string }) => {
   return (
     <div className="w-full px-16 md:px-32 xl:px-64 2xl:px-96 py-6 flex justify-between items-center">
       <h1 className="text-3xl font-extrabold hover:text-zinc-600 dark:hover:text-zinc-300">
-        <a href="/">{title}</a>
+        <Link href="/">{title}</Link>
       </h1>
       <SwitchTheme />
     </div>
