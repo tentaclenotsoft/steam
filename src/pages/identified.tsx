174c016919e6e30f3adf596cad2abef77cc75ea7
ruj <214782@bk.ru>
2022-05-03 00:11:28 -0300

    chore: clipboard icons grayed

--- src/pages/identified.tsx
+++ src/pages/identified.tsx
@@ -197,7 +197,7 @@ const Identified: NextPage = () => {
                           <span>{value}</span>
                           <CopyToClipboard text={value}>
                             <button
-                              className="grid content-center"
+                              className="grid content-center text-zinc-600 dark:text-zinc-300"
                               onClick={() =>
                                 Toast({
                                   type: EToastType.SUCCESS,
