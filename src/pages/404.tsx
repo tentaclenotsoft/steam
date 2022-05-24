dacc2615ce3cb81e9354af5775580186f40a9b39
ruj <214782@bk.ru>
2022-05-24 09:25:55 -0300

    chore: translations for page not found

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
 
-import type { NextPage } from 'next'
+import type { GetStaticPropsContext } from 'next'
+import { useTranslations } from 'next-intl'
 import Link from 'next/link'
+
+import pick from 'lodash/pick'
+import { NextPageWithMessages } from 'types'
 
 import Footer from '@components/Footer'
 
-const NotFound: NextPage = () => {
+const NotFound: NextPageWithMessages = () => {
+  const t = useTranslations('NotFound')
+
   return (
     <div className="h-screen flex flex-col justify-between">
       <div />
       <div className="flex justify-center">
         <div className="flex flex-col items-center text-center space-y-3">
-          <h1 className="text-5xl font-extrabold">Page not found.</h1>
+          <h1 className="text-5xl font-extrabold">{t('title')}</h1>
           <button className="h-10 w-fit px-3 font-semibold text-white bg-zinc-600 dark:bg-zinc-800 hover:bg-zinc-400 hover:dark:bg-zinc-600">
-            <Link href="/">Go back home</Link>
+            <Link href="/">{t('goBackHome')}</Link>
           </button>
         </div>
       </div>
@@ -21,5 +27,16 @@ const NotFound: NextPage = () => {
     </div>
   )
 }
+
+NotFound.messages = ['NotFound', 'Footer']
+
+export const getStaticProps = async ({ locale }: GetStaticPropsContext) => ({
+  props: {
+    messages: pick(
+      await import(`../assets/json/locales/${locale}.json`),
+      NotFound.messages
+    )
+  }
+})
 
 export default NotFound
