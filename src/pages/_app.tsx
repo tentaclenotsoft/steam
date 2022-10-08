e3bf8d3045a7b2bdf8cda99535e160bbf53a5b11
ruj <214782@bk.ru>
2022-10-08 20:24:49 -0300

    chore: get layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ToastContainer } from 'react-toastify'
 
+import { NextComponentType } from 'next'
 import { NextIntlProvider } from 'next-intl'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
@@ -10,18 +11,29 @@ import '../styles/globals.css'
 import '../components/leveled/SteamLevels/levels.css'
 import 'react-toastify/dist/ReactToastify.css'
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </Head>
-    <NextIntlProvider messages={pageProps.messages}>
-      <ThemeProvider attribute="class" enableSystem={false}>
-        <Component {...pageProps} />
-        <ToastContainer />
-      </ThemeProvider>
-    </NextIntlProvider>
-  </>
-)
+type CustomAppProps = AppProps & {
+  Component: NextComponentType & {
+    getLayout: () => void
+  }
+}
+
+const MyApp = ({ Component, pageProps }: CustomAppProps) => {
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <NextIntlProvider messages={pageProps.messages}>
+        <ThemeProvider attribute="class" enableSystem={false}>
+          {getLayout(<Component {...pageProps} />)}
+          <ToastContainer />
+        </ThemeProvider>
+      </NextIntlProvider>
+    </>
+  )
+}
 
 export default MyApp
