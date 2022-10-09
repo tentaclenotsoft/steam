e5ceb3d65672f8e04f4300b7f610249dee0197de
ruj <214782@bk.ru>
2022-10-08 21:24:33 -0300

    fix: intl messages typing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
 import { ToastContainer } from 'react-toastify'
 
-import { NextComponentType } from 'next'
-import { NextIntlProvider } from 'next-intl'
+import { NextComponentType, NextPageContext } from 'next'
+import { AbstractIntlMessages, NextIntlProvider } from 'next-intl'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
@@ -12,8 +11,11 @@ import '../components/leveled/SteamLevels/levels.css'
 import 'react-toastify/dist/ReactToastify.css'
 
 type CustomAppProps = AppProps & {
-  Component: NextComponentType & {
+  Component: NextComponentType<NextPageContext, any, any> & {
     getLayout: () => void
+  }
+  pageProps: {
+    messages: AbstractIntlMessages
   }
 }
 
@@ -25,7 +27,6 @@ const MyApp = ({ Component, pageProps }: CustomAppProps) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-
       <NextIntlProvider messages={pageProps.messages}>
         <ThemeProvider attribute="class" enableSystem={false}>
           {getLayout(<Component {...pageProps} />)}
