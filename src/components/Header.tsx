e53f8c93a7dabd3efb4aa0496a77a7c424b9faea
ruj <214782@bk.ru>
2023-01-10 19:37:39 -0300

    chore: aesthetic and responsive improvements and new menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,42 @@
+import Link from 'next/link'
+
+import clsx from 'clsx'
+
 import SwitchTheme from './SwitchTheme'
+
+const MenuItem = ({ href, text }: { href?: string; text: string }) => {
+  return (
+    <div
+      className={clsx(
+        'font-light',
+        'text-zinc-600 dark:text-zinc-300 hover:text-black hover:dark:text-white',
+        'ease-in-out duration-300',
+        'uppercase cursor-pointer'
+      )}
+    >
+      {href ? <Link href={href}>{text}</Link> : text}
+    </div>
+  )
+}
 
 export default function Header () {
   return (
-    <div className="h-60 sm:h-44 flex flex-col sm:flex-row justify-between items-center px-16 py-6">
-      <div className="text-center text-5xl font-black tracking-tighter leading-9 bg-clip-text text-transparent bg-gradient-to-br from-rose-600 to-red-700">
-        Steam
-        <br />
-        Toolkit
-      </div>
-      <div className="w-9/12 flex justify-center items-center text-xl space-x-5">
-        <div className="text-extrabold hover:scale-125 cursor-pointer ease-in-out duration-300">
-          Foo
+    <div className="flex flex-col sm:flex-row justify-between items-center px-16 py-6 space-y-5 sm:space-y-0">
+      <Link href="/">
+        <div
+          className={clsx(
+            'text-center text-5xl font-black tracking-tighter leading-9',
+            'bg-clip-text text-transparent bg-gradient-to-br from-rose-600 to-red-700 hover:from-rose-700 hover:to-red-800',
+            'cursor-pointer'
+          )}
+        >
+          Steam
+          <br />
+          Toolkit
         </div>
-        <div className="text-extrabold hover:scale-125 cursor-pointer ease-in-out duration-300">
-          Bar
-        </div>
-        <div className="text-extrabold hover:scale-125 cursor-pointer ease-in-out duration-300">
-          Qux
-        </div>
+      </Link>
+      <div className="w-9/12 flex justify-center items-center space-x-5">
+        <MenuItem href="/level" text="Level" />
       </div>
       <SwitchTheme />
     </div>
