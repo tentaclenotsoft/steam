9387cfcbc9b4f3ea37d2f75b438a98eecf5d63fb
ruj <214782@bk.ru>
2023-02-05 16:28:03 -0300

    chore: size scale

--- src/components/SteamLevels/index.tsx
+++ src/components/SteamLevels/index.tsx
@@ -2,11 +2,22 @@ import clsx from 'clsx'
 
 import { levelToClasses } from '@utils'
 
-export default function SteamLevels ({ level }: { level: number }) {
+export default function SteamLevels ({
+  level,
+  size
+}: {
+  level: number
+  size?: string
+}) {
   const levelClasses = levelToClasses(level)
+  const sizes = {
+    small: null,
+    medium: 'scale-125',
+    large: 'scale-[3]'
+  }
 
   return (
-    <div className={clsx('level', levelClasses?.join(' ').trim(), 'scale-[3]')}>
+    <div className={clsx('level', levelClasses?.join(' ').trim(), sizes[size])}>
       <div className="h-full flex justify-center">
         <span
           className={clsx(
@@ -14,7 +25,7 @@ export default function SteamLevels ({ level }: { level: number }) {
             !level ? 'text-[#9b9b9b]/50' : 'text-white'
           )}
         >
-          {level ?? '?'}
+          {level || '?'}
         </span>
       </div>
       <div className="absolute inset-0 -z-10 bg-zinc-400/50 dark:bg-transparent blur" />
